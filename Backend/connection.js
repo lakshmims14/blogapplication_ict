1d8fde9cdfad1d73da8e723f65f9e7ad961eb043
lakshmi_14 <lakshmims1402@gmail.com>
2025-06-29 16:39:03 +0530

    Update connection.js

--- Backend/connection.js
+++ Backend/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 //Write missing code here
 mongoose
-  .connect("mongodb+srv://username:password@cluster0.zzl6qou.mongodb.net/ict?retryWrites=true&w=majority&appName=Cluster0")
+  .connect("mongodb+srv://usernamee:password@cluster0.zzl6qou.mongodb.net/ict?retryWrites=true&w=majority&appName=Cluster0")
   .then(() => {
     console.log("Connected to DB");
   })
